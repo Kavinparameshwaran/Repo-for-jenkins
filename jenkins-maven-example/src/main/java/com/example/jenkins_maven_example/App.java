ed4774e0c535b64d345e5db807054a12ee418c9f
AD18820 <AD18820@IND-5CG2216WT7.ctl.intranet>
2022-09-22 16:48:38 +0530

    Newly created jenkins

---
+++ jenkins-maven-example/src/main/java/com/example/jenkins_maven_example/App.java
@@ -0,0 +1,14 @@
+package com.example.jenkins_maven_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Jenkins!!!!!!!!!");
+    }
+}
